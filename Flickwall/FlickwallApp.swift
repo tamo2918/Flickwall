78b1c956d08f91784119892172ababd13a41c3c5
tamo2918 <mrt.t0304@gmail.com>
2026-05-22 09:30:08 +0900

    Initial Commit

---
+++ Flickwall/FlickwallApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlickwallApp.swift
+//  Flickwall
+//
+//  Created by Tatsuki Morita on 2026/05/22.
+//
+
+import SwiftUI
+
+@main
+struct FlickwallApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ FlickwallTests/FlickwallTests.swift
@@ -0,0 +1,17 @@
+//
+//  FlickwallTests.swift
+//  FlickwallTests
+//
+//  Created by Tatsuki Morita on 2026/05/22.
+//
+
+import Testing
+@testable import Flickwall
+
+struct FlickwallTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
